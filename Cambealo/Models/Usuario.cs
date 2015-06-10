69c2ef17826b33b2cb82fa882dc5dce4d059180c
bnalvarado <bnalvarado94@gmail.com>
2015-06-09 21:05:45 -0700

    primer commit, creacion del projecto y modelo usuario con su respectivo controlador y migracion

---
+++ Cambealo/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cambealo.Models
+{
+    public class Usuario
+    {
+        public int id { set; get; }
+        public String nombre { set; get; }
+        public String apellidos { set; get; }
+        public String correo { set; get; }
+        public String contraseña { set; get; }
+        public int telefono { set; get; }
+        public int edad { set; get; }
+    }
+}
---
+++ Cambealo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cambealo.Startup))]
+namespace Cambealo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
