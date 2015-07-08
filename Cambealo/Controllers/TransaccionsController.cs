ec51e5b0a6118ed2672227a11fb331d2cf44daad
luisgzvs <luisgzvs@gmail.com>
2015-07-08 15:18:19 -0600

    transaction
    Creation of model, controller and view of transactions

--- Cambealo/Controllers/TransaccionsController.cs
+++ Cambealo/Controllers/TransaccionsController.cs
@@ -10,117 +10,109 @@ using Cambealo.Models;
 
 namespace Cambealo.Controllers
 {
-    public class UsuariosController : Controller
+    public class TransaccionsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Usuarios
+        // GET: Transaccions
         public ActionResult Index()
         {
-            return View(db.Usuarios.ToList());
+            return View(db.Transaccions.ToList());
         }
 
-        // GET: Usuarios/Details/5
+        // GET: Transaccions/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
+            Transaccion transaccion = db.Transaccions.Find(id);
+            if (transaccion == null)
             {
                 return HttpNotFound();
             }
-            return View(usuario);
+            return View(transaccion);
         }
 
-        // GET: Usuarios/Create
+        // GET: Transaccions/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Usuarios/Create
+        // POST: Transaccions/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Nombre,Apellidos,Correo,Contraseña,ConfirmarContraseña,Telefono,Edad")] Usuario usuario)
+        public ActionResult Create([Bind(Include = "Id,Ofrecido,Interes,Fecha,Estado,IdUsuario")] Transaccion transaccion)
         {
             if (ModelState.IsValid)
             {
-                db.Usuarios.Add(usuario);
+                db.Transaccions.Add(transaccion);
                 db.SaveChanges();
-                TempData["message"] = "Usuario creado con éxito";
-                TempData["created"] = true;
-                return RedirectToAction("Create");
+                return RedirectToAction("Index");
             }
-            TempData["message"] = "El usuario no pudo ser creado";
-            TempData["created"] = false;
-            return RedirectToAction("Create");
+
+            return View(transaccion);
         }
 
-        // GET: Usuarios/Edit/5
+        // GET: Transaccions/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
+            Transaccion transaccion = db.Transaccions.Find(id);
+            if (transaccion == null)
             {
                 return HttpNotFound();
             }
-            return View(usuario);
+            return View(transaccion);
         }
 
-        // POST: Usuarios/Edit/5
+        // POST: Transaccions/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nombre,Apellidos,Correo,Contraseña,Telefono,Edad")] Usuario usuario)
+        public ActionResult Edit([Bind(Include = "Id,Ofrecido,Interes,Fecha,Estado,IdUsuario")] Transaccion transaccion)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(usuario).State = EntityState.Modified;
+                db.Entry(transaccion).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(usuario);
+            return View(transaccion);
         }
 
-        // GET: Usuarios/Delete/5
+        // GET: Transaccions/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Usuario usuario = db.Usuarios.Find(id);
-            if (usuario == null)
+            Transaccion transaccion = db.Transaccions.Find(id);
+            if (transaccion == null)
             {
                 return HttpNotFound();
             }
-            return View(usuario);
+            return View(transaccion);
         }
 
-        // POST: Usuarios/Delete/5
+        // POST: Transaccions/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Usuario usuario = db.Usuarios.Find(id);
-            db.Usuarios.Remove(usuario);
+            Transaccion transaccion = db.Transaccions.Find(id);
+            db.Transaccions.Remove(transaccion);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
-
-        public ActionResult Productos()
-        {
-            return Redirect("/Productos/Create/");
         }
 
         protected override void Dispose(bool disposing)
